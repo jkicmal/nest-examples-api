3a001279005e9eed945b62aad4ebf3f0005f0ea6
Jakub <jakub.kicmal@gmail.com>
2021-07-19 15:28:05 +0200

    feat: add .env.example

--- src/modules/auth/commands/signup/signup.dto.ts
+++ src/modules/auth/commands/signup/signup.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class SignupDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
